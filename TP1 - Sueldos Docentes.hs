db2978c9fe4c818cbeec2538c1a30564b6d9985c
Valentin Ennio Bravo <valentinenniob@gmail.com>
2024-04-11 18:40:03 -0300

    Agrego tp sueldos docentes

---
+++ TP1 - Sueldos Docentes.hs
@@ -0,0 +1,20 @@
+basicoPorCargo :: String -> Float
+basicoPorCargo x | x=="titular" = 149000
+                 | x=="adjunto" = 116000
+                 | x=="ayudante"= 66000
+                 | otherwise = 0
+
+porcentajeIncremento :: Float -> Float
+porcentajeIncremento x | x>=24 = 2.2
+                       | x>=10 = 1.5
+                       | x>=5 = 1.3
+                       | x>=3 = 1.2
+                       | otherwise = 1
+
+cantHoras :: Float -> Int
+cantHoras x = round (x/10)
+
+sueldo :: String -> Float -> Float -> Float
+sueldo x y z = basicoPorCargo x * porcentajeIncremento y * fromIntegral (cantHoras z)
+
+
